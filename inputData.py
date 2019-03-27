c2039f74eeb87bfc3fab789c89488b1e2cb26b3e
voblacksmith <vokuznetcov@gmail.com>
2019-03-27 18:11:00 +0300

    Added exploratory (Jupyter)

--- inputData.py
+++ inputData.py
@@ -1,26 +1,45 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from pandas.api.types import CategoricalDtype
+from sklearn.model_selection import train_test_split
 
 
-train = pd.read_csv('train_data.csv')
-train_df = pd.DataFrame(train)
-test = pd.read_csv('test_data.csv')
-test_df = pd.DataFrame(test)
+load = pd.read_csv('train_data.csv')
+df = pd.DataFrame(load)
 
-X_train = train_df.iloc[:,0:145]
-y_train = train_df.iloc[:,145]
-X_test = test_df.iloc[:,0:145]
-y_test = test_df.iloc[:,145]
+X_df = df.iloc[:,0:145]
+y_df = df.iloc[:,145]
 
-X_train_np = X_train.as_matrix()
-y_train_np = y_train.as_matrix()
+X_train, X_test, y_train, y_test = train_test_split(
+    X_df, y_df, test_size=0.25, random_state=64
+)
 
-#print(X_train_np[:,0].shape)
-print(y_train_np.shape)
+print(X_train.shape)
+print(X_test.shape)
 
-fig = plt.figure()
-plt.scatter(X_train_np[:,70], y_train_np, s=1)
-plt.show()
+cat_type = CategoricalDtype(categories = ["low", "moderate", "high"], ordered=True)
+X_train["gamma_ray"] = X_train["gamma_ray"].astype(cat_type)
+X_train["gamma_ray"] = X_train["gamma_ray"].cat.rename_categories([1,2,3])
+X_test["gamma_ray"] = X_test["gamma_ray"].astype(cat_type)
+X_test["gamma_ray"] = X_test["gamma_ray"].cat.rename_categories([1,2,3])
 
-#print(train_df[train_df.year > 2050])
+
+X_train = X_train.values
+y_train = y_train.values
+X_test = X_test.values
+y_test = y_test.values
+
+#for i in range(0, 145, 1):
+    #print(type(X_train_np[0,i]))
+
+
+lr = LinearRegression().fit(X_train, y_train)
+
+print("Train Score: {}".format(lr.score(X_train, y_train)))
+print("Test Score: {}".format(lr.score(X_test, y_test)))
+
+#fig = plt.figure()
+#plt.scatter(X_train_np[:,70], y_train_np, s=1)
+#plt.show()
